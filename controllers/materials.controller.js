964445640ff0d6a3740f9ef6089dbf0385c07f31
Ali <aliumarov90@mail.ru>
2022-04-02 16:54:04 +0300

    доработал поступлении материала и вывод отчета по месяца

--- controllers/materials.controller.js
+++ controllers/materials.controller.js
@@ -1,51 +1,73 @@
-const Material = require('../models/Material.model')
+const Material = require("../models/Material.model");
 
 module.exports.materialsController = {
-    addMaterials: async (req, res) => {
-        const { name, price, volumeType, left, direction } = req.body;
-        try {
-            const material = await Material.create({
-                name,
-                price,
-                volumeType,
-                left,
-                direction
-            })
-            res.json(material)
-        }catch (e) {
-            res.json(e.message)
-        }
-    },
-    getAllMaterials: async (req, res) => {
-        try {
-            const materials = await Material.find()
-            res.json(materials)
-        } catch (e) {
-            res.json(e.message)
-        }
-    },
-    removeMaterial: async (req, res) => {
-        const {id} = req.params
-        try {
-            const removeMaterial = await Material.findByIdAndDelete(id)
-            res.json(removeMaterial)
-        }catch (e) {
-            res.json(e.message);
-        }
-    },
-    addingMaterial: async (req, res) => {
-        const {volume} = req.body
-        try {
-            const increment = await Material.findByIdAndUpdate(req.params.id, {
-                $push: {direction: {volume, date: Date()}},
-            }, {new: true})
-            res.json(increment)
-        } catch (e) {
-            res.json("error" + e.toString())
-        }
+  addMaterials: async (req, res) => {
+    const { name, price, volumeType, left, direction } = req.body;
+    try {
+      const material = await Material.create({
+        name,
+        price,
+        volumeType,
+        left,
+        direction,
+      });
+      res.json(material);
+    } catch (e) {
+      res.json(e.message);
     }
-}
+  },
+  getAllMaterials: async (req, res) => {
+    try {
+      const materials = await Material.find();
+      res.json(materials);
+    } catch (e) {
+      res.json(e.message);
+    }
+  },
+  removeMaterial: async (req, res) => {
+    const { id } = req.params;
+    try {
+      const removeMaterial = await Material.findByIdAndDelete(id);
+      res.json(removeMaterial);
+    } catch (e) {
+      res.json(e.message);
+    }
+  },
+  addingMaterial: async (req, res) => {
+    const { volume } = req.body;
+    try {
+      const increment = await Material.findByIdAndUpdate(
+        req.params.id,
+        {
+          $push: { direction: { volume, date: new Date() } },
+          $inc: { left: +volume },
+        },
+        { new: true }
+      );
+      res.json(increment);
+    } catch (e) {
+      res.json("error" + e.toString());
+    }
+  },
 
-
-
-
+  getAllMaterialsForThePeriod: async (req, res) => {
+    try {
+      const getForThePeriod = await Material.aggregate([
+        {
+          $group: {
+            _id: {month: {$month: '$date'}},
+          totalPrice: {
+              $sum: '$price'
+          },
+            name: {
+              $addToSet: '$name'
+            }
+          }
+        }
+      ]);
+      res.json(getForThePeriod);
+    } catch (e) {
+      res.json("error" + e.toString());
+    }
+  },
+};
