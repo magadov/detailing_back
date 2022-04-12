38f17b4d613b0ea785292c49400897b1f14a8105
Ali <aliumarov90@mail.ru>
2022-04-12 14:25:46 +0300

    приготовление к ПЛ

--- routes/car.route.js
+++ routes/car.route.js
@@ -6,11 +6,6 @@ const authMiddleware = require("../middlewares/auth.middleware");
 const router = Router();
 
 router.get("/", authMiddleware, carsController.getCar);
-router.post("/", authMiddleware, carsController.addCar);
-router.delete("/:id", authMiddleware, carsController.deleteCar);
-router.patch("/add", authMiddleware, carsController.addVinData);
-router.get("/get", authMiddleware, carsController.getVin);
-router.patch("/update", authMiddleware, carsController.updateCar);
 
 
 module.exports = router;
