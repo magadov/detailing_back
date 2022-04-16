ab5fd5998bcf7f9d366dca97f9ca9b4ea70b61b8
usman <dzakaev.94@mail.ru>
2022-04-16 15:07:44 +0300

    исправил контроллеры admins,cars,clients

--- routes/admins.route.js
+++ routes/admins.route.js
@@ -3,9 +3,7 @@ const { adminsController } = require("../controllers/admins.controller");
 
 const router = Router();
 
-router.get("/", adminsController.getAdmin);
 router.post("/", adminsController.login);
 // router.post('/admin', adminsController.addAdmin);
-router.delete("/:id", adminsController.removeAdmin);
 
 module.exports = router;
