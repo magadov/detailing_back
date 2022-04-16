8997f61b0fea8deecf82c43ad156b67ea2a2407f
Ali <aliumarov90@mail.ru>
2022-04-16 13:29:41 +0300

    исправил пути в material.routes

--- routes/materials.route.js
+++ routes/materials.route.js
@@ -3,11 +3,11 @@ const { materialsController } = require("../controllers/materials.controller");
 
 const router = Router();
 
-router.post('/add', materialsController.addMaterials);
-router.get('/get', materialsController.getAllMaterials);
-router.delete('/remove/:id', materialsController.removeMaterial);
-router.patch('/adding/:id', materialsController.addingMaterial);
-router.patch('/consumption/:id', materialsController.consumptionMaterial);
-router.get('/get/forThePeriod', materialsController.getAllMaterialsForThePeriod);
+router.post('/', materialsController.addMaterials);
+router.get('/', materialsController.getAllMaterials);
+router.delete('/:id', materialsController.removeMaterial);
+router.patch('/:id', materialsController.addingMaterial);
+router.patch('/:id/consumption', materialsController.consumptionMaterial);
+router.get('/byDate', materialsController.getAllMaterialsForThePeriod);
 
 module.exports = router;
