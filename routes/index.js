379ca0ff989a78ad4224eea40fdf86ab7de533e3
Vis <awwya@mail.ru>
2022-04-16 13:27:40 +0300

    Вынес строку соединения в ENV файл

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,10 @@ const { Router } = require("express");
 
 const router = Router();
 
-router.use('/services', require('./services.route'));
-router.use('/materials', require('./materials.route'))
-router.use('/admins', require('./admins.route'))
-router.use('/clients', require('./clients.route'))
-router.use('/cars', require('./car.route'))
-
-
+router.use("/services", require("./services.route"));
+router.use("/materials", require("./materials.route"));
+router.use("/admins", require("./admins.route"));
+router.use("/clients", require("./clients.route"));
+router.use("/cars", require("./car.route"));
 
 module.exports = router;
